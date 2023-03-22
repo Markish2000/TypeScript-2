be57d626f94160e54e235fc29f3788b157d19ccb
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-03-22 15:51:40 -0300

    Hero

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 // import { Hero as SuperHero } from './classes/Hero';
-import * as HeroClasses from './classes/Hero';
-import powers from './data/powers';
+// import * as HeroClasses from './classes/Hero';
+import { Hero } from './classes/Hero';
 
-const Hero = 123;
-
-const ironMan = new HeroClasses.Hero('Iron Man', 1, 55);
+const ironMan = new Hero('Iron Man', 1, 55);
 console.log(ironMan);
-console.log(powers);
+console.log(ironMan);
