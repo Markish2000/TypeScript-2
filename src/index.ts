87daaa33013f4385707dfc8755af8ced36037178
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-03-28 01:16:23 -0300

    Cosas

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { Hero } from './interfaces/hero';
+import { Villain } from './interfaces/villain';
 import {
   printObject,
   genericFunction,
@@ -9,8 +11,16 @@ import {
 // printObject({ a: 1, b: 2, c: 3 });
 // printObject([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 // printObject('Hola Mundo');
-const name: string = 'Marcos';
+// const name: string = 'Marcos';
 
-console.log(genericFunctionArrow(3.141618).toFixed(2));
-console.log(genericFunctionArrow(name).toLowerCase());
-console.log(genericFunctionArrow(new Date()).getDate());
+// console.log(genericFunctionArrow(3.141618).toFixed(2));
+// console.log(genericFunctionArrow(name).toLowerCase());
+// console.log(genericFunctionArrow(new Date()).getDate());
+
+const deadpool = {
+  name: 'Deadpool',
+  realName: 'Wade Winston Wilson',
+  dangerLevel: 130,
+};
+
+console.log(genericFunctionArrow<Villain>(deadpool).dangerLevel);
